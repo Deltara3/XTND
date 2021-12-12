342b717d63079695e9ec1239f1228d1dc88f1f3e
zTags <wierdgeomery@gmail.com>
2021-12-12 19:48:16 +0100

    ok everything works now

--- lib/src/lib.rs
+++ lib/src/lib.rs
@@ -1,5 +1,8 @@
+extern crate libc;
+extern crate ctor;
+
 mod unsafes;
-use unsafes::{print, println};
+use unsafes::println;
 
 use std::fs::read_dir;
 
@@ -7,24 +10,22 @@ use ctor::{ctor, dtor};
 
 #[ctor]
 fn init() {
-    println("XTND injected.");
+    println!("XTND injected.");
     
-    println("Loading modules...");
+    println!("Loading modules...");
     // TODO implement this
     load_modules();
 
-    println("XTND loaded.");
+    println!("XTND loaded.");
 }
 
 fn load_modules() {
     // TODO are you implementing this yet
-    println("Discovering modules...");
+    println!("Discovering modules...");
     let paths = read_dir("~/.xtnd").unwrap();
 
     for path in paths {
-        print("Dicovered plugin ");
-        print(format!("{}", path.unwrap().path().display()));
-        print("\n");
+        println!("Dicovered plugin {}", path.unwrap().path().display());
     }
 }
 
